a453cf21d82305abbee1aa96de1de4a1eceb1bb2
Faisal Hasnain <faisal@faisalhasnain.com>
2018-09-29 22:06:01 +0500

    upgraded codes for apollo-server-lambda 2.1.0

--- handler.js
+++ handler.js
@@ -1,31 +1,7 @@
-import { graphqlLambda, graphiqlLambda } from 'apollo-server-lambda';
-import { makeExecutableSchema } from 'graphql-tools';
-
+import { ApolloServer } from 'apollo-server-lambda';
 import schema from './schema.graphql';
 import { resolvers } from './resolvers';
 
-const myGraphQLSchema = makeExecutableSchema({
-  typeDefs: schema,
-  resolvers,
-  logger: console,
-});
+const server = new ApolloServer({ typeDefs: schema, resolvers });
 
-export function graphqlHandler(event, context, callback) {
-  context.callbackWaitsForEmptyEventLoop = false;
-  console.log('Executing GraphQL handler...');
-  function callbackFilter(error, output) {
-    if (!output.headers) {
-      output.headers = {};
-    }
-    output.headers['Access-Control-Allow-Origin'] = '*';
-    callback(error, output);
-  }
-  const handler = graphqlLambda({ schema: myGraphQLSchema });
-  return handler(event, context, callbackFilter);
-};
-
-export function graphiqlHandler(event, context, callback) {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const endpointURL = 'graphql';
-  return graphiqlLambda({ endpointURL })(event, context, callback);
-};
+exports.graphqlHandler = server.createHandler();
